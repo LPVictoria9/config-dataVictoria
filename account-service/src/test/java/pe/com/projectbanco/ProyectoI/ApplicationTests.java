47ce4f6cedc6e8c92ecf9ee1bcdc98da2417e905
vlarraur <victoria.larrauripizarro@gmail.com>
2022-08-25 16:46:35 -0500

    add refrencias eureka client y eureka Server

--- account-service/src/test/java/pe/com/projectbanco/ProyectoI/ApplicationTests.java
+++ account-service/src/test/java/pe/com/projectbanco/ProyectoI/ApplicationTests.java
@@ -3,6 +3,7 @@ package pe.com.projectbanco.ProyectoI;
 //import org.junit.Test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
 class ApplicationTests {
--- product-service/src/main/java/pe/com/projectbanco/ProyectoI/impl/ProductServiceImpl.java
+++ product-service/src/main/java/pe/com/projectbanco/ProyectoI/impl/ProductServiceImpl.java
@@ -55,4 +55,9 @@ public class ProductServiceImpl implements IProductrService {
         return iProductRepo.findAll().filter(p->p.getNameProduct().equals(nameProduct));
 
     }
+
+    @Override
+    public Flux<Product> findByCodProduct(String codProduct) {
+        return null;
+    }
 }
