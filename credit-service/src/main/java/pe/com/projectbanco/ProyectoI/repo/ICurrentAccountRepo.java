ae0c83333aa247377a0603f6c6719c039924c43a
vlarraur <victoria.larrauripizarro@gmail.com>
2022-09-02 17:32:01 -0500

    update proyects

--- credit-service/src/main/java/pe/com/projectbanco/ProyectoI/repo/ICurrentAccountRepo.java
+++
@@ -1,7 +0,0 @@
-package pe.com.projectbanco.ProyectoI.repo;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import pe.com.projectbanco.ProyectoI.model.currentAccount;
-
-public interface ICurrentAccountRepo extends ReactiveMongoRepository<currentAccount, String> {
-}
--- credit-service/src/main/java/pe/com/projectbanco/ProyectoI/service/ICurrentAccountService.java
+++
@@ -1,6 +0,0 @@
-package pe.com.projectbanco.ProyectoI.service;
-
-import pe.com.projectbanco.ProyectoI.model.currentAccount;
-
-public interface ICurrentAccountService extends  ICRUD<currentAccount, String>{
-}
